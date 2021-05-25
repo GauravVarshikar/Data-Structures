2b714984d8edea562628e8f6e3100ed6d562cade
Gaurav <gaurav.varshikar@gmail.com>
2021-05-25 11:56:38 +0530

    Google question. Find the first recurring character in an array

--- src/HashTables/FirstRecurringChar.java
+++ src/HashTables/FirstRecurringChar.java
@@ -20,11 +20,9 @@ public class FirstRecurringChar {
 
         nums = new int[]{1,2,3,4,5,4,2,3};
         System.out.println(findFirstRecurringChar(nums));
+        System.out.println(firstRecurringCharQuadratic(nums));
 
         nums = new int[]{1,2,3,4,5};
-        System.out.println(findFirstRecurringChar(nums));
-
-        nums = new int[]{1};
         System.out.println(findFirstRecurringChar(nums));
     }
 
@@ -40,6 +38,21 @@ public class FirstRecurringChar {
                 numSet.add(num);
             }
         }
+        return null;
+    }
+
+    private static Integer firstRecurringCharQuadratic(int[] nums){
+        int lowIndex = nums.length;
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                if(nums[i] == nums[j]){
+                    if(j<lowIndex){
+                        lowIndex = j;
+                    }
+                }
+            }
+        }
+        if(lowIndex != nums.length) return nums[lowIndex];
         return null;
     }
 }
