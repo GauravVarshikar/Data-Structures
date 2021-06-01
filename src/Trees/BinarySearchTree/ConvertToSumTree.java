09faecb0d05dacd5d8ce15770a2307379de365bb
Gaurav <gaurav.varshikar@gmail.com>
2021-06-01 11:53:10 +0530

    Check if a tree is a SUM TREE or not

--- src/Trees/BinarySearchTree/ConvertToSumTree.java
+++ src/Trees/BinarySearchTree/ConvertToSumTree.java
@@ -18,6 +18,38 @@ public class ConvertToSumTree {
         System.out.println("After converting the tree to its sum tree: ");
         convertToSumTree(root);
         bst.display();
+
+        //Check if the given binary tree is a sum tree or not
+        Node root1 = new Node(44);
+        root1.left = new Node(9);
+        root1.right = new Node(13);
+        root1.left.left = new Node(4);
+        root1.left.right = new Node(5);
+        root1.right.left = new Node(6);
+        root1.right.right = new Node(7);
+        bst.root = root1;
+        bst.display();
+        if (isSumTree(root1) == Integer.MIN_VALUE) {
+            System.out.println("NOT a sum tree");
+        } else {
+            System.out.println("It is a sum tree");
+        }
+    }
+
+    private static int isSumTree(Node node) {
+        if(node == null){
+            return 0;
+        }
+        if(node.left == null && node.right == null){
+            return node.value;
+        }
+        int leftNodeVal = isSumTree(node.left);
+        int rightNodeVal = isSumTree(node.right);
+        System.out.println(node.value + " - " + leftNodeVal + " + " + rightNodeVal);
+        if(node.value == (leftNodeVal + rightNodeVal)){
+            return node.value + node.value;
+        }
+        return Integer.MIN_VALUE;
     }
 
     private static int convertToSumTree(Node node) {
