54036a8f5d4ada6637495165dc7b82a31743b0b6
Gaurav <gaurav.varshikar@gmail.com>
2021-06-02 18:45:10 +0530

    Construct tree from a given Inorder and PreOrder, from Inorder and PostOrder, and from PreOrder and PostOrder

--- src/Trees/BinarySearchTree/TreeFromTraversal.java
+++ src/Trees/BinarySearchTree/TreeFromTraversal.java
@@ -22,36 +22,66 @@ public class TreeFromTraversal {
         System.out.println("Tree created from In and Pre Order");
         bst.display();
 
-        //Construct tree from InOrder and PreOrder traversals
+        //Construct tree from InOrder and PostOrder traversals
         tree = constructTree(inOrder, postOrder, IN_ORDER, POST_ORDER);
         bst = new BinarySearchTree();
         bst.root = tree;
         System.out.println("Tree created from In and Post Order");
         bst.display();
 
+        //Construct tree from PreOrder and PostOrder traversals
+        int[] preOrder2 = { 1, 2, 4, 5, 3, 6, 8, 9, 7 };
+        int[] postOrder2 = { 4, 5, 2, 8, 9, 6, 7, 3, 1 };
+        if(preOrder2.length == postOrder2.length && preOrder2.length%2 == 1) {
+            tree = constructTree(postOrder2, preOrder2, PRE_ORDER, POST_ORDER);
+        }
+        bst = new BinarySearchTree();
+        bst.root = tree;
+        System.out.println("Tree created from Pre and Post Order");
+        bst.display();
+
     }
 
-    private static Node constructTree(int[] inOrder, int[] order2, String tra1, String tra2) {
-         Map<Integer, Integer> inOrderMap = new HashMap<>();
+    private static Node constructTree(int[] order1, int[] order2, String tra1, String tra2) {
+         Map<Integer, Integer> order1Map = new HashMap<>();
          //Store all the values of the InOrder array in a map
-         for(int i = 0; i<inOrder.length; i++){
-             inOrderMap.put(inOrder[i], i);
+         for(int i = 0; i<order1.length; i++){
+             order1Map.put(order1[i], i);
          }
          AtomicInteger index;
          if(tra1.equals(IN_ORDER) && tra2.equals(PRE_ORDER)) {
              index = new AtomicInteger(0); //in preorder traversal first node is the root node
-             return constructTreeFromInAndPre(0, inOrder.length - 1, order2, inOrderMap, index);
-         }
-         else if(tra1.equals(IN_ORDER) && tra2.equals(POST_ORDER)) {
-             index = new AtomicInteger(inOrder.length-1); //in postorder last node is the root node
-             return constructTreeFromInAndPost(0, inOrder.length - 1, order2, inOrderMap, index);
+             return constructTreeFromInAndPre(0, order1.length - 1, order2, order1Map, index);
+         } else if(tra1.equals(IN_ORDER) && tra2.equals(POST_ORDER)) {
+             index = new AtomicInteger(order1.length-1); //in postorder last node is the root node
+             return constructTreeFromInAndPost(0, order1.length - 1, order2, order1Map, index);
+         } else if(tra1.equals(PRE_ORDER) && tra2.equals(POST_ORDER)) {
+             index = new AtomicInteger(0); //in preorder first node is the root node
+             return constructTreeFromPreAndPost(0, order1.length - 1, order2, order1Map, index);
          }
          return null;
     }
 
-    // int[] inOrder = { 4, 2, 1, 7, 5, 8, 3, 6 };
-    // int[] postOrder = { 4, 2, 7, 8, 5, 6, 3, 1 };
-    /*
+    //int[] preOrder2 = { 1, 2, 4, 5, 3, 6, 8, 9, 7 };
+    //        int[] postOrder2 = { 4, 5, 2, 8, 9, 6, 7, 3, 1 };
+    private static Node constructTreeFromPreAndPost(int start, int end, int[] preOrder, Map<Integer, Integer> order1Map,
+                                                    AtomicInteger index) {
+        Node root = new Node(preOrder[index.getAndIncrement()]);
+        if(index.get() == preOrder.length){
+            return root;
+        }
+        int newIndex = order1Map.get(preOrder[index.get()]);
+
+        if (start <= newIndex && newIndex + 1 <= end - 1) {
+            root.left = constructTreeFromPreAndPost(start, newIndex, preOrder, order1Map, index);
+            root.right = constructTreeFromPreAndPost(newIndex + 1, end - 1, preOrder, order1Map, index);
+        }
+        return root;
+    }
+
+    /* int[] inOrder = { 4, 2, 1, 7, 5, 8, 3, 6 };
+       int[] postOrder = { 4, 2, 7, 8, 5, 6, 3, 1 };
+
     * The idea is to start with the root node, which would be the last item in the postorder sequence, and find the
     * boundary of its left and right subtree in the inorder sequence. To find the boundary, search for the index of the
     * root node in the inorder sequence. All keys before the root node in the inorder sequence become part of the left
